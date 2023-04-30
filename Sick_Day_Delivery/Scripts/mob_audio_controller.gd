71238134b68f3d5c1d9cd3f2d19511938dff1cec
Turner Bass <turner@astropanda.studio>
2023-04-30 11:40:35 -0500

    Add mob audio controller

---
+++ Sick_Day_Delivery/Scripts/mob_audio_controller.gd
@@ -0,0 +1,43 @@
+extends Node2D
+class_name MobAudioController
+
+@export var next_behavior_min: float = 4.0
+@export var next_behavior_max: float = 10.0
+@export var enabled:bool = true
+
+@export var actions: Array[AudioStream]
+@export var behaviors: Array[AudioStream]
+
+@onready var behavior_timer = $BehaviorTimer
+@onready var behavior_player = $BehaviorAudioPlayer
+@onready var action_player = $ActionAudioPlayer
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pause()
+	reset_behavior_timer()
+
+func reset_behavior_timer():
+	behavior_timer.wait_time = randf_range(next_behavior_min, next_behavior_max)
+	behavior_timer.start()
+
+func _on_BehaviorTimer_timeout():
+	if behaviors.size() > 0 && enabled:
+		reset_behavior_timer()
+		var behavior: AudioStream = behaviors[randi() % behaviors.size()]
+		behavior_player.stream = behavior
+		behavior_player.play()
+
+func act(index: int):
+	if enabled:
+		var action: AudioStream = actions[clamp(index, 0, actions.size())]
+		action_player.stream = action
+		action_player.play()
+	
+func play():
+	action_player.playing = true
+	behavior_player.playing = true
+	
+func pause():
+	action_player.playing = false
+	behavior_player.playing = false
