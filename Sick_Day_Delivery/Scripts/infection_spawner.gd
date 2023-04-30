71ebcd8ba0531712cc49298f39504c1c4fd90832
Caleb Haldane <caldane@gmail.com>
2023-04-29 21:46:44 -0500

    Add colony spawner

--- Sick_Day_Delivery/Scripts/infection_spawner.gd
+++ Sick_Day_Delivery/Scripts/infection_spawner.gd
@@ -1,8 +1,11 @@
-extends Node
+extends Timer
 
 @export var colony_count := 1
 @export_range(0, 20, 2) var spawn_rate: int
 @export_range(0, 1, .025) var spawn_chance: float
+@export var colony: PackedScene
+
+var spawn_positions = [ Vector2(0,0), Vector2(50,50) ]
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -11,4 +14,6 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	var colony = colony.instantiate()
+	colony.position = spawn_positions[0]
+	add_child(colony)
