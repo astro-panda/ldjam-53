fde566d8b0e618ab5826305c519cd98c04538f52
Caleb Haldane <Caleb.Haldane@loves.com>
2023-05-01 13:23:41 -0500

    Add maximum generation count to coloy spawning

--- Sick_Day_Delivery/Scripts/infection_spawner.gd
+++ Sick_Day_Delivery/Scripts/infection_spawner.gd
@@ -1,12 +1,14 @@
 extends Timer
 
-@export var colony_max := 1
+@export var colony_max_on_screen := 1
+@export var colony_max_generations := 12
+
 @export_range(0, 20, 2) var spawn_rate: int
 @export_range(0, 1, .025) var spawn_chance: float
 @export var colonyScene: PackedScene
 
 @onready var spawns = $SpawnPoints.get_children()
-
+var colony_generation = 1
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -18,16 +20,18 @@ func _process(delta):
 	pass
 
 func createColony():
-	var child_count = 0
-	for child in $Colonies.get_children():
-		if child.process_priority != 0:
-			child_count += 1
-	if child_count < colony_max:
-		var colony = colonyScene.instantiate()
-		var current_colony_name = spawns[0].name
-		while current_colony_name == spawns[0].name:
-			spawns.shuffle()
-		print_debug(spawns[0].name + ": " + str(spawns[0].position))
-		colony.process_priority = 1
-		colony.position = spawns[0].position		
-		$Colonies.add_child(colony)
+	if colony_generation < colony_max_generations:
+		var child_count = 0
+		for child in $Colonies.get_children():
+			if child.process_priority != 0:
+				child_count += 1
+		if child_count < colony_max_on_screen:
+			colony_generation += 1
+			var colony = colonyScene.instantiate()
+			var current_colony_name = spawns[0].name
+			while current_colony_name == spawns[0].name:
+				spawns.shuffle()
+			print_debug(spawns[0].name + ": " + str(spawns[0].position))
+			colony.process_priority = 1
+			colony.position = spawns[0].position		
+			$Colonies.add_child(colony)
