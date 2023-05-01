38c71e19aeb5abaf93cef6609d576696f6f5467b
Hope-Parnell <hope@astropanda.studio>
2023-05-01 11:55:48 -0500

    Create Win Overlay

--- Sick_Day_Delivery/Scripts/main.gd
+++ Sick_Day_Delivery/Scripts/main.gd
@@ -7,17 +7,23 @@ func _ready():
 	$PauseMenu.process_mode = Node.PROCESS_MODE_ALWAYS
 	$PlayerCell.process_mode = Node.PROCESS_MODE_INHERIT
 	$HUD.process_mode = Node.PROCESS_MODE_INHERIT
+	$GameOver.process_mode = Node.PROCESS_MODE_INHERIT
 
 func _on_global_state_on_game_over():
 	print("game over 😭")
-	game_over()
+	game_end()
+	$GameOver.game_over($HUD/Time/TimeAlive.text)
 	
 func _process(delta):
 	if Input.is_action_just_pressed("game_over"):
-		game_over()
+		game_end()
+		$GameOver.game_over($HUD/Time/TimeAlive.text)
+	if Input.is_action_just_pressed("win"):
+		$PauseMenu.process_mode = Node.PROCESS_MODE_DISABLED
+		$Win.win($HUD/Time/TimeAlive.text)
 
-func game_over():
+func game_end():
 	$PauseMenu.process_mode = Node.PROCESS_MODE_DISABLED
 	$PlayerCell.process_mode = Node.PROCESS_MODE_DISABLED
 	$HUD.process_mode = Node.PROCESS_MODE_DISABLED
-	$GameOver.game_over($HUD/Time/TimeAlive.text)
+	$HUD.visible = false
