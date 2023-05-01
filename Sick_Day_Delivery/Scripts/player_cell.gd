f104d1fc080b69829710650f3338c52241c26659
Turner Bass <turner@astropanda.studio>
2023-05-01 15:32:33 -0500

    add air brake mechanic

--- Sick_Day_Delivery/Scripts/player_cell.gd
+++ Sick_Day_Delivery/Scripts/player_cell.gd
@@ -1,29 +1,32 @@
 extends CharacterBody2D
 
-@export var acceleration := 2.0
-@export var max_speed := 400.0
-@export var rotation_speed := 250.0
 @export var cureScene: PackedScene
+
+@export var acceleration: float = 2.0
+@export var max_speed: float = 400.0
+@export var rotation_speed: float = 250.0
+@export var decceleration: float = .01
+
 
 func _ready():
 	$PlayerAnimation.play()
 
-
 func _physics_process(delta):
-	var input_vector := Vector2(0, Input.get_axis("move_forward", "move_backward"))
+	var input_vector = Vector2(0, Input.get_axis("move_forward", "move_backward"))	
 	
-	velocity += input_vector.rotated(rotation) * acceleration
+	if Input.is_action_pressed("brake"):
+		velocity = velocity.lerp(Vector2.ZERO, decceleration)
+
+	velocity += input_vector.normalized().rotated(rotation) * acceleration
 	velocity = velocity.limit_length(max_speed)
 	
 	if Input.is_action_pressed("rotate_right"):
 		rotate(deg_to_rad(rotation_speed * delta))
 	if Input.is_action_pressed("rotate_left"):
 		rotate(deg_to_rad(-rotation_speed * delta))
-	
-	if input_vector.y == 0:
-		velocity = velocity.move_toward(Vector2.ZERO, 3)
-	
+
 	move_and_slide()
+	
 	for i in get_slide_collision_count():
 		var collision = get_slide_collision(i)
 		if(collision.get_collider().has_method("destroy_bacteria")):
