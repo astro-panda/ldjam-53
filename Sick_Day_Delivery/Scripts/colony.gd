72e8ab39ea5000c8b1b7b4607265a8ce8bf0e3be
Semnor <leithprise@gmail.com>
2023-04-30 21:42:08 -0500

    add enemy movement

--- Sick_Day_Delivery/Scripts/colony.gd
+++ Sick_Day_Delivery/Scripts/colony.gd
@@ -2,7 +2,7 @@ extends Area2D
 
 signal enemy_spawn
 
-@export var bacteria_count := 1
+@export var bacteria_max := 100
 @export_range(0, 20, 2) var spawn_rate: int
 @export_range(0, 1, .025) var spawn_chance: float
 @export var bacteriumScene: PackedScene
@@ -25,8 +25,7 @@ func _process(delta):
 		queue_free()
 		
 func createBacterium():
-	if process_priority > 0:
-		print_debug("spawn bacterium")
+	if process_priority > 0 && get_children().size() -3 < bacteria_max:
 		var bacterium = bacteriumScene.instantiate()
 		bacterium.position = Vector2.ZERO
 		add_child(bacterium)
