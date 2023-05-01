bd2594ebc1f9252419e54e5b7fe6cb2e5b0eb8f7
Turner Bass <turner@astropanda.studio>
2023-04-30 23:09:42 -0500

    uppercase Player on bacteria indicator

--- Sick_Day_Delivery/Scripts/bacteria_indicator.gd
+++ Sick_Day_Delivery/Scripts/bacteria_indicator.gd
@@ -1,6 +1,6 @@
 extends Node2D
 
-@onready var player = get_tree().get_first_node_in_group("player")
+@onready var player = get_tree().get_first_node_in_group("Player")
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
