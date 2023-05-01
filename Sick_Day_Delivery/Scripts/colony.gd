201f35c54a5434e75cba89504dcfbbfd2f491b2f
Semnor <leithprise@gmail.com>
2023-04-30 20:21:25 -0500

    add random spawning locations

--- Sick_Day_Delivery/Scripts/colony.gd
+++ Sick_Day_Delivery/Scripts/colony.gd
@@ -1,4 +1,4 @@
-extends Node2D
+extends Area2D
 
 signal enemy_spawn
 
@@ -7,6 +7,9 @@ signal enemy_spawn
 @export_range(0, 1, .025) var spawn_chance: float
 @export var bacteriumScene: PackedScene
 
+var colliding = false
+var colony_ded = false
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
@@ -14,14 +17,31 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
-
+	if Input.is_action_pressed("cure") && colliding:
+		$ColonySprite.hide()
+		process_priority = 0
+	if self.get_children().size() == 3 && process_priority == 0:
+		print_debug("colony destroyed")
+		queue_free()
+		
 func createBacterium():
-	var bacterium = bacteriumScene.instantiate()
-	bacterium.position = Vector2.ZERO
-	add_child(bacterium)
-
+	if process_priority > 0:
+		print_debug("spawn bacterium")
+		var bacterium = bacteriumScene.instantiate()
+		bacterium.position = Vector2.ZERO
+		add_child(bacterium)
 
 func _on_colony_timer_timeout():
-	print_debug("spawn bacterium")
+	
 	createBacterium()
+
+func destroy_colony():
+	queue_free()
+	
+func _on_body_entered(body):
+	if body.is_in_group("Player"):
+		colliding = true
+
+func _on_body_exited(body):
+	if body.is_in_group("Player"):
+		colliding = false
--- Sick_Day_Delivery/Scripts/player_cell.gd
+++ Sick_Day_Delivery/Scripts/player_cell.gd
@@ -24,16 +24,5 @@ func _physics_process(delta):
 	
 	move_and_slide()
 	
-#	var screen_size = get_viewport_rect().size
-#	if global_position.y < 0:
-#		global_position.y = screen_size.y
-#	elif global_position.y > screen_size.y:
-#		global_position.y = 0
-#	if global_position.x < 0:
-#		global_position.x = screen_size.x
-#	elif global_position.x > screen_size.x:
-#		global_position.x = 0	
-		
-#	position += velocity * delta
-#	position.x = clamp(position.x, 0, screen_size.x)
-#	position.y = clamp(position.y, 0, screen_size.y)
+	
+
