198c706980429e248f1c7af5b96ec8fcff276cba
Semnor <leithprise@gmail.com>
2023-04-30 20:21:25 -0500

    add random spawning locations

--- Sick_Day_Delivery/Scripts/infection_spawner.gd
+++ Sick_Day_Delivery/Scripts/infection_spawner.gd
@@ -18,8 +18,16 @@ func _process(delta):
 	pass
 
 func createColony():
-	if $Colonies.get_children().size() < colony_max:
+	var child_count = 0
+	for child in $Colonies.get_children():
+		if child.process_priority != 0:
+			child_count += 1
+	if child_count < colony_max:
 		var colony = colonyScene.instantiate()
-		print_debug(spawns[0].position)
+		var current_colony_name = spawns[0].name
+		while current_colony_name == spawns[0].name:
+			spawns.shuffle()
+		print_debug(spawns[0].name + ": " + str(spawns[0].position))
+		colony.process_priority = 1
 		colony.position = spawns[0].position
 		$Colonies.add_child(colony)
