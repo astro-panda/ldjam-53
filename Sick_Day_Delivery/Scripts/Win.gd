6209b1fda6887f07ce62a3a547f3ed94f601d711
Hope-Parnell <hope@astropanda.studio>
2023-05-01 11:55:48 -0500

    Create Win Overlay

--- Sick_Day_Delivery/Scripts/Win.gd
+++ Sick_Day_Delivery/Scripts/Win.gd
@@ -10,13 +10,15 @@ func _ready():
 func _process(delta):
 	pass
 
-func game_over(time):
+func win(time):
 	$Canvas/Message.text = "Bacterium Killed: %d\nColonies Destroyed: %d\nElapsed Time: %s" %[GlobalState.enemies_destroyed, GlobalState.colonies_destroyed, time]
 	$Canvas.visible = true
+	get_tree().paused = true
 
 
 func _on_play_again_pressed():
 	GlobalState.reset_state()
+	get_tree().paused = false
 	get_tree().reload_current_scene()
 
 
